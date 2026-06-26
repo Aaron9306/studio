c5c3999a7d0fbd223fb95f7fc357ca881df10656
A96 <90542458+Aaron9306@users.noreply.github.com>
2026-06-26 22:07:57 +0400

    Delete src/supabase.ts

--- src/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createBrowserClient } from '@supabase/ssr';
-
-// Supabase project credentials
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-// Create a browser client for client-side components
-export const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey);
